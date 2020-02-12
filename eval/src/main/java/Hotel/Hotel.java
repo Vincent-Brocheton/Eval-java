acea7a0d41edaed77f3e7b596a78e194599bf64b
Vincent.Brocheton <vinc.brocheton@gmail.com>
2020-02-12 16:55:16 +0100

    Contructeur Hotel

--- eval/src/main/java/Hotel/Hotel.java
+++ eval/src/main/java/Hotel/Hotel.java
@@ -4,9 +4,15 @@ import java.util.ArrayList;
 
 public class Hotel extends ArrayList {
     public String nomHotel;
-    public int nbChambre;
+    public int nbChambres;
     public ArrayList<Chambre> chambres;
+    public int etages;
 
-    
+    public Hotel(String nomHotel, int nbChambres, int nbEtages){
+        this.nomHotel = nomHotel;
+        this.nbChambres = nbChambres;
+        this.etages = nbEtages;
+        
+    }
     
 }
